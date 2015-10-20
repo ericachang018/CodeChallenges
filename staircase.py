51556f67bb24ea185b161f4bb38e39ffcbf70427
Erica Chang <Erica@Ericas-MBP.home>
2015-10-19 23:52:54 -0400

    code challenge make a staircase with hash tags

---
+++ staircase.py
@@ -0,0 +1,28 @@
+# input is an int 
+# output is a staircase of # 
+# Staircase to the left output:
+     #
+    ## 
+   ###
+  ####
+ #####
+######
+
+def staircase_to_right(nums):
+	steps = range(0,nums +1)
+	print steps
+	for x in steps:
+		print "#" * x
+
+def staircase_to_left(nums):
+	steps = range(1, nums + 1)
+	print steps
+	for x in steps: 
+		spaces = steps[-1] - x
+		hashes = steps[x - 1]
+		print " " * spaces + "#" * hashes	 
+
+
+staircase_to_left(6)
+
+# staircase_to_right(6)
